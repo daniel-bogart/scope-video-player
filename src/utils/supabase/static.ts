a35abf21c17174b4a9a43e5af2dfceb66d768415
Daniel Bogart <75914647+daniel-bogart@users.noreply.github.com>
2024-08-19 16:46:17 -0700

    get initial supabase setup

---
+++ src/utils/supabase/static.ts
@@ -0,0 +1,11 @@
+// utils/supabase/static.ts
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+import { Database } from "../../types/supabase";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createSupabaseClient<Database>(
+  supabaseUrl,
+  supabaseKey
+);
